004189c77b1978f9f2576665c6452be27395ef32
shrushti0066 <shrushti0066@gmail.com>
2026-06-24 18:22:50 +0530

    Add text splitting functionality using RecursiveCharacterTextSplitter

--- chunk.py
+++ chunk.py
@@ -1,1 +1,13 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+
+def split_text(text, chunk_size, chunk_overlap):
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
+    )
+
+    chunks = splitter.split_text(text)
+
+    return chunks
